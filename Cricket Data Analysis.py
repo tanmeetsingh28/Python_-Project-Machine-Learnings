c578cb71e4ea34bedc9155db744ef1de9438c2e5
tanmeetsingh28 <147396176+tanmeetsingh28@users.noreply.github.com>
2023-10-09 16:54:12 +0530

    commit

---
+++ Cricket Data Analysis.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Project 
+
+# In[2]:
+
+
+import pandas as pd 
+import plotly.express as px 
+import plotly.graph_objects as go 
+
+
+# In[9]:
+
+
+datac = pd.read_csv("E:\\New folder\\Cricketipl.csv")
+
+
+# In[10]:
+
+
+datac
+
+
+# In[ ]:
+
+
+Nu,ber 
+
